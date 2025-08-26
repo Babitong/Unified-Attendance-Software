75a32b07ef30c99de878c48e4f7b6b842fd3232d
Babitong <rexbabitong@gmail.com>
2025-08-26 20:36:27 +0100

    preparing for hosting in render

--- Unified_ATTENDANCE_SYSTEM/settings.py
+++ Unified_ATTENDANCE_SYSTEM/settings.py
@@ -64,6 +64,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -153,6 +154,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'frontend',
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
